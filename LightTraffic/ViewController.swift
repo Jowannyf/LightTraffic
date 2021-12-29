80f89b801a37e0a4cbd55304ad8ea9b4ea903ae5
Ivan <zhukkivan51@gmail.com>
2021-12-29 17:39:33 +0300

    Ориентировано на большинство устройств, но не решена проблема с округлением вьюшкек.

--- LightTraffic/ViewController.swift
+++ LightTraffic/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    @IBOutlet var changeLightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.layer.cornerRadius = redLightView.frame.width / 2
+        redLightView.alpha = 0.5
+        
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.width / 2
+        yellowLightView.alpha = 0.5
+        
+        greenLightView.layer.cornerRadius = greenLightView.frame.width / 2
+        greenLightView.alpha = 0.5
+        
+        changeLightButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func switchLightButton(_ sender: Any) {
+        if redLightView.alpha == 0.5 && yellowLightView.alpha == 0.5 {
+            greenLightView.alpha = 0.5
+            redLightView.alpha = 1
+        } else if yellowLightView.alpha == 0.5 && greenLightView.alpha == 0.5 {
+            redLightView.alpha = 0.5
+            yellowLightView.alpha = 1
+        } else if greenLightView.alpha == 0.5 && redLightView.alpha == 0.5 {
+            yellowLightView.alpha = 0.5
+            greenLightView.alpha = 1
+        }
+        changeLightButton.setTitle("Next", for: .normal)
+    }
+    
 }
 
